1aa880fc88bd8749c5e1c6f80c40c03cb1c1ca23
Aryan <69156357+Aryan6600@users.noreply.github.com>
2022-02-18 14:21:42 +0530

    Improved Design and Optimised Speed

--- Pages/Home.js
+++ Pages/Home.js
@@ -2,8 +2,8 @@ const Router = require('express').Router()
 const Blogs = require('../database/Schemas/Blogs.js')
 
 Router.get('/', async (req, res) => {
-    const latest = await Blogs.findOne().sort("-time")
-    const blogs = await Blogs.find().sort("-time").skip(1).limit(15)
+    const latest = await Blogs.findOne().sort("-time").select("thumbnail title description")
+    const blogs = await Blogs.find().sort("-time").skip(1).select("thumbnail title description").limit(15)
     res.render('index.ejs', { blogs, latest });
 })
 
