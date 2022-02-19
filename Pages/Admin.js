9f9fbf8360413814fa44e9f8e9d1a043b1ce7734
Aryan <69156357+Aryan6600@users.noreply.github.com>
2022-02-19 12:06:49 +0530

    Optimised Code for Admin

--- Pages/Admin.js
+++ Pages/Admin.js
@@ -4,37 +4,40 @@ const BcryptJs = require("bcryptjs");
 const Blogs = require("../database/Schemas/Blogs");
 const { body, validationResult } = require("express-validator");
 
-Router.get("/", (req, res) => {
-    res.render("Admin.login.ejs");
-});
+// Router For / 
 
-Router.post(
-    "/",
-    [body("user").isString(), body("pass").isString()],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        const { user, pass } = req.body;
-        try {
-            const exist = await Admins.findOne().where("username").equals(user);
-            if (exist) {
-                const admin = await Admins.findOne({ username: user });
-                const verify = await BcryptJs.compare(pass, admin.password);
-                if (verify) {
-                    req.session.user = admin;
-                    res.redirect("/admin/dashboard");
-                } else {
-                    res.redirect("/admin");
-                    console.log("Password is incorrect");
-                }
+Router.route("/")
+    .get((req, res) => {
+        res.render("Admin.login.ejs");
+    })
+    .post([body("user").isString(), body("pass").isString()],
+        async (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
             }
-        } catch (error) {
-            res.redirect("/admin");
+            const { user, pass } = req.body;
+            try {
+                const exist = await Admins.findOne().where("username").equals(user);
+                if (exist) {
+                    const admin = await Admins.findOne({ username: user });
+                    const verify = await BcryptJs.compare(pass, admin.password);
+                    if (verify) {
+                        req.session.user = admin;
+                        res.redirect("/admin/dashboard");
+                    } else {
+                        res.redirect("/admin");
+                        console.log("Password is incorrect");
+                    }
+                }
+            } catch (error) {
+                res.redirect("/admin");
+            }
         }
-    }
-);
+    );
+
+
+// Router For /dashboard
 
 Router.get("/dashboard", async (req, res) => {
     if (!req.session.user) { return res.redirect("/admin"); }
@@ -42,53 +45,57 @@ Router.get("/dashboard", async (req, res) => {
     res.render("Admin.home.ejs", { blogs: blogs, admin: req.session.user })
 });
 
-Router.get("/compose", (req, res) => {
-    if (!req.session.user) {
-        return res.redirect("/admin");
-    }
-    res.render("Admin.compose.ejs", { admin: req.session.user });
-});
+// Router For /compose
 
-Router.post("/compose", (req, res) => {
-    if (!req.session.user) {
-        return res.redirect("/admin");
-    }
-    const { title, description, body, thumbnail } = req.body;
-    const blog = new Blogs({
-        title,
-        description,
-        body,
-        thumbnail,
-    });
-    blog.save().then(() => {
-        res.redirect("/blog/" + blog.slug);
-    });
-});
-
-Router.get('/edit/:slug', async (req, res) => {
-    if (!req.session.user) return res.redirect('/admin')
-    try {
-        const blog = await Blogs.findById(req.params.slug)
-        res.render('Admin.edit.ejs', { blog: blog, admin: req.session.user })
-    } catch (error) {
-        res.redirect('/admin/dashboard')
-    }
-})
-
-Router.post('/edit/:id', async (req, res) => {
-    if (!req.session.user) return res.redirect('/admin')
-    try {
+Router.route("/compose")
+    .get((req, res) => {
+        if (!req.session.user) {
+            return res.redirect("/admin");
+        }
+        res.render("Admin.compose.ejs", { admin: req.session.user });
+    })
+    .post((req, res) => {
+        if (!req.session.user) {
+            return res.redirect("/admin");
+        }
         const { title, description, body, thumbnail } = req.body;
-        const blog = await Blogs.findById(req.params.id)
-        blog.title = title;
-        blog.description = description;
-        blog.body = body;
-        blog.thumbnail = thumbnail;
-        await blog.save()
-        res.redirect('/blog/' + blog._id)
-    } catch (error) {
-        res.redirect('/admin/dashboard')
-    }
-})
+        const blog = new Blogs({
+            title,
+            description,
+            body,
+            thumbnail,
+        });
+        blog.save().then(() => {
+            res.redirect("/blog/" + blog.slug);
+        });
+    })
+
+// Router for Editing
+Router.route("/edit/:slug")
+    .get(async (req, res) => {
+        if (!req.session.user) return res.redirect('/admin')
+        try {
+            const blog = await Blogs.findById(req.params.slug)
+            res.render('Admin.edit.ejs', { blog: blog, admin: req.session.user })
+        } catch (error) {
+            res.redirect('/admin/dashboard')
+        }
+    })
+    .post(async (req, res) => {
+        if (!req.session.user) return res.redirect('/admin')
+        try {
+            const { title, description, body, thumbnail } = req.body;
+            const blog = await Blogs.findById(req.params.slug)
+            blog.title = title;
+            blog.description = description;
+            blog.body = body;
+            blog.thumbnail = thumbnail;
+            await blog.save()
+            res.redirect('/blog/' + blog.slug)
+        } catch (error) {
+            res.redirect('/admin/dashboard')
+        }
+    })
+
 
 module.exports = Router;
